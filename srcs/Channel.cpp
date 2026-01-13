ba85ec7d6429e9f35074e3be983f7fd06d04bd9a
yeonjunky <yeonjunky@gmail.com>
2026-01-13 11:23:21 +0900

    add function signitures

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -1,1 +1,23 @@
 #include "Channel.hpp"
+
+Channel::Channel() {}
+
+Channel::~Channel() {}
+
+void Channel::addUser(std::string nickname) {}
+void Channel::delUser(std::string nickname) {}
+std::unordered_map<std::string, MemberInfo> Channel::getUsers() const {}
+
+int Channel::promoteToOp(std::string prompter, std::string target) {}
+
+void Channel::setInvite(bool value) {}
+bool Channel::getInvite() const {}
+void Channel::inviteUser(std::string nickname) {}
+
+void Channel::setTopicMode(bool value) {}
+bool Channel::getTopicMode() const {}
+void Channel::setTopic(std::string topic) {}
+std::string Channel::getTopic() const {}
+
+void Channel::setKey(std::string newKey) {}
+std::string Channel::getKey() const {}
