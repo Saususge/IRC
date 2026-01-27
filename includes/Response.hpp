7bc1458606cc1584d48a2971f4edb9e01b16e128
KHR416 <hyy741@naver.com>
2026-01-27 23:52:52 +0900

    Replace class to namespace

--- includes/Response.hpp
+++ includes/Response.hpp
@@ -2,18 +2,18 @@
 #define RESPONSE_HPP
 
 #include <string>
-#include <iostream>
-#include "numeric.hpp"
 
-class Response {
- public:
-  static std::string build(const std::string& code, const std::string& target, const std::string& message) {
-    return ":" + std::string("irc.local") + " " + code + " " + target + " " + message + "\r\n";
-  }
-  
-  static std::string error(const std::string& code, const std::string& target, const std::string& message) {
-     return build(code, target, message);
-  }
-};
+namespace Response {
+std::string build(const std::string& code, const std::string& target,
+                  const std::string& message) {
+  return ":" + std::string("irc.local") + " " + code + " " + target + " " +
+         message + "\r\n";
+}
+
+std::string error(const std::string& code, const std::string& target,
+                  const std::string& message) {
+  return build(code, target, message);
+}
+};  // namespace Response
 
 #endif
