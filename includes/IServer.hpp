8d690cd36e9b47204b3dc458f08ec97b4a63bde6
KHR416 <hyy741@naver.com>
2026-01-29 03:28:06 +0900

    Modified IServer

--- includes/IServer.hpp
+++ includes/IServer.hpp
@@ -1,26 +1,17 @@
 #ifndef ISERVER_HPP
 #define ISERVER_HPP
 
-#include <string>
-#include <vector>
-
-#include "IChannel.hpp"
-#include "IClient.hpp"
+#include "IChannelResistry.hpp"
+#include "IClientRegistry.hpp"
 #include "ISession.hpp"
 
 class IServer {
  public:
   virtual ~IServer() {};
 
-  virtual void registerClient(IClient* client) = 0;
-  virtual bool isNickInUse(const std::string& nick) = 0;
-  virtual const IClient* findClient(const std::string& nick) = 0;
-  virtual std::vector<const IClient*> getAllClients() = 0;
+  virtual IClientRegistry& clients() = 0;
 
-  virtual int createChannel(const std::string& channelName) = 0;
-  virtual int deleteChannel(const std::string& channelName) = 0;
-  virtual IChannel* findChannel(const std::string& channelName) = 0;
-  virtual std::vector<const IChannel*> getChannels() = 0;
+  virtual IChannelResistry channels() = 0;
 
   virtual int acceptSession() = 0;
   // Defererd disconnection
