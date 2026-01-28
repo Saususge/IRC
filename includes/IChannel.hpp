0fb6a94d823a664ccd2a1829adfefca7815aafad
KHR416 <hyy741@naver.com>
2026-01-29 02:54:12 +0900

    Declare IChannel

---
+++ includes/IChannel.hpp
@@ -0,0 +1,37 @@
+#ifndef ICHANNEL_HPP
+#define ICHANNEL_HPP
+
+#include <string>
+#include <vector>
+
+class IChannel {
+ public:
+  // Bit mask
+  typedef int IChannelMode;
+
+  virtual ~IChannel() {};
+
+  virtual const std::string& getChannelName() const = 0;
+
+  // Use `except` only for exclusing the sender
+  virtual int broadcast(const std::string& msg,
+                        const std::string& except = "") = 0;
+
+  virtual int addClient(const std::string& nick) = 0;
+  virtual void removeClient(const std::string& nick) = 0;
+  virtual bool hasClient(const std::string& nick) const = 0;
+  virtual int setClientOp(const std::string& nick) = 0;
+  virtual int unsetClientOp(const std::string& nick) = 0;
+  virtual std::vector<const std::string&> getAllClients() = 0;
+
+  virtual int getClientNumber() const = 0;
+
+  virtual int setMode(IChannelMode mode) = 0;
+  virtual int addMode(IChannelMode mode) = 0;
+  virtual int removeMode(IChannelMode mode) = 0;
+
+  virtual int addToInviteList(const std::string& nick) = 0;
+  virtual int removeFromInviteList(const std::string& nick) = 0;
+  virtual bool isInInviteList(const std::string& nick) const = 0;
+};
+#endif
