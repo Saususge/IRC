a098f3ad0c8661aabe8e097036c41a003e79d561
KHR416 <hyy741@naver.com>
2026-01-29 12:42:56 +0900

    Add missing operations

--- includes/IChannelResistry.hpp
+++ includes/IChannelResistry.hpp
@@ -28,6 +28,35 @@ class IChannelResistry {
                          const std::string requesterNick,
                          IChannel::IChannelMode mode) = 0;
 
+  virtual int removeClient(const std::string& channelName,
+                           const std::string& nick) = 0;
+  virtual bool hasClient(const std::string& channelName,
+                         const std::string& nick) const = 0;
+  virtual int isClientOp(const std::string& channelName,
+                         const std::string& nick) const = 0;
+  virtual const std::vector<const std::string>& getClients() = 0;
+
+  virtual int getClientNumber(const std::string& channelName) const = 0;
+
+  virtual int setMode(const std::string& channelName,
+                      const std::string& reqeusterNcik,
+                      IChannel::IChannelMode mode) = 0;
+  virtual int addMode(const std::string& channelName,
+                      const std::string& reqeusterNcik,
+                      IChannel::IChannelMode mode) = 0;
+  virtual int removeMode(const std::string& channelName,
+                         const std::string& reqeusterNcik,
+                         IChannel::IChannelMode mode) = 0;
+
+  virtual int addToInviteList(const std::string& channelName,
+                              const std::string& requesterNcik,
+                              const std::string& targetNick) = 0;
+  virtual int removeFromInviteList(const std::string& channelName,
+                                   const std::string& requesterNcik,
+                                   const std::string& targetNick) = 0;
+  virtual bool isInInviteList(const std::string& channelName,
+                              const std::string& nick) const = 0;
+
   // Use `except` only if excluding sender.
   virtual int broadcast(const std::string& channelName, const std::string& msg,
                         const std::string& except = "") = 0;
