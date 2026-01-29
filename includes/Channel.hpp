9c1faa9ca5e4a2bf831f6df1092d5ea1170a04e3
yeonjunky <yeonjunky@gmail.com>
2026-01-29 17:36:57 +0900

    update signiture Channel

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -11,27 +11,32 @@ class Channel : private IChannel {
   const std::string& getChannelName() const;
 
   // Use `except` only for exclusing the sender
-  int broadcast(const std::string& msg, const std::string& except = "");
+  IRC::Numeric broadcast(const std::string& msg,
+                                 const std::string& except = "");
 
-  int addClient(const std::string& nick);
-  void removeClient(const std::string& nick);
+  IRC::Numeric addClient(const std::string& nick);
+  IRC::Numeric removeClient(const std::string& nick);
   bool hasClient(const std::string& nick) const;
-  int setClientOp(const std::string& nick);
-  int unsetClientOp(const std::string& nick);
-  int isClientOp(const std::string& nick) const;
+  IRC::Numeric setClientOp(const std::string& requesterNick,
+                                   const std::string& targetNick);
+  IRC::Numeric unsetClientOp(const std::string& requesterNick,
+                                     const std::string& targetNick);
+  bool isClientOp(const std::string& nick) const;
   const std::vector<const std::string>& getClients();
 
   int getClientNumber() const;
 
-  int setMode(const std::string& reqeusterNick, IChannelMode mode);
-  int addMode(const std::string& reqeusterNick, IChannelMode mode);
-  int removeMode(const std::string& reqeusterNick, IChannelMode mode);
-  IChannelMode getMode() const;
+  IRC::Numeric setMode(const std::string& requesterNick,
+                               IChannelMode mode);
+  IRC::Numeric addMode(const std::string& requesterNick,
+                               IChannelMode mode);
+  IRC::Numeric removeMode(const std::string& requesterNick,
+                                  IChannelMode mode);
 
-  int addToInviteList(const std::string& requesterNick,
-                      const std::string& targetNick);
-  int removeFromInviteList(const std::string& requesterNick,
-                           const std::string& targetNick);
+  IRC::Numeric addToInviteList(const std::string& requesterNick,
+                                       const std::string& targetNick);
+  IRC::Numeric removeFromInviteList(const std::string& requesterNick,
+                                            const std::string& targetNick);
   bool isInInviteList(const std::string& nick) const;
 
  private:
