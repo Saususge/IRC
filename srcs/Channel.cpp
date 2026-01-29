0fc1ca77474c8796e8ff0cad9f629f97e777d04a
yeonjunky <yeonjunky@gmail.com>
2026-01-29 19:46:38 +0900

    temp commit

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -10,13 +10,28 @@ int Channel::broadcast(const std::string& msg, const std::string& except = "") {
   return 1;
 }
 
-int Channel::addClient(const std::string& nick) {
-  if (joinedUsers.find(nick) != joinedUsers.end()) return 0;
+IRC::Numeric Channel::addClient(const std::string& nick,
+                                const std::string& key) {
+  if (joinedUsers.find(nick) != joinedUsers.end())
+    return IRC::DO_NOTHING;
+  else if (this->mode & 0b10 && invitedUsers.find(nick) == invitedUsers.end()) {
+    return IRC::ERR_INVITEONLYCHAN;
+  } else if (!this->key.empty() && this->key != key) {
+    return IRC::ERR_BADCHANNELKEY;
+  } else if (this->limit != 0 && this->limit <= this->joinedUsers.size()) {
+    return IRC::ERR_CHANNELISFULL;
+  }
+
   joinedUsers.insert(nick);
-  return 1;
+  this->mode & 0b10 ? this->invitedUsers.erase(nick) : 0;
+
+  return topic.empty() ? IRC::RPL_NOTOPIC : IRC::RPL_TOPIC;
 }
 
-IRC::Numeric Channel::removeClient(const std::string& nick) {}
+IRC::Numeric Channel::removeClient(const std::string& nick) {
+  if (joinedUsers.find(nick) == joinedUsers.end()) return IRC::ERR_NOTONCHANNEL;
+  joinedUsers.erase(nick);
+}
 
 bool Channel::hasClient(const std::string& nick) const {}
 
@@ -42,9 +57,9 @@ IRC::Numeric Channel::removeMode(const std::string& reqeusterNick,
                                  IChannelMode mode) {}
 
 IRC::Numeric Channel::addToInviteList(const std::string& requesterNick,
-                             const std::string& targetNick) {}
+                                      const std::string& targetNick) {}
 
 IRC::Numeric Channel::removeFromInviteList(const std::string& requesterNick,
-                                  const std::string& targetNick) {}
+                                           const std::string& targetNick) {}
 
 bool Channel::isInInviteList(const std::string& nick) const {}
