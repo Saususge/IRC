b0f0a9d37f26d66ff9c4529f14bd553e6584c167
yeonjunky <yeonjunky@gmail.com>
2026-01-29 19:22:22 +0900

    update function signatures in Channel class to return IRC::Numeric type

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -16,32 +16,35 @@ int Channel::addClient(const std::string& nick) {
   return 1;
 }
 
-void Channel::removeClient(const std::string& nick) {}
+IRC::Numeric Channel::removeClient(const std::string& nick) {}
 
 bool Channel::hasClient(const std::string& nick) const {}
 
-int Channel::setClientOp(const std::string& nick) {}
+IRC::Numeric Channel::setClientOp(const std::string& nick,
+                                  const std::string& targetNick) {}
 
-int Channel::unsetClientOp(const std::string& nick) {}
+IRC::Numeric Channel::unsetClientOp(const std::string& nick,
+                                    const std::string& targetNick) {}
 
-int Channel::isClientOp(const std::string& nick) const {}
+bool Channel::isClientOp(const std::string& nick) const {}
 
 const std::vector<const std::string>& Channel::getClients() {}
 
 int Channel::getClientNumber() const {}
 
-int Channel::setMode(const std::string& reqeusterNick, IChannelMode mode) {}
+IRC::Numeric Channel::setMode(const std::string& reqeusterNick,
+                              IChannelMode mode) {}
 
-int Channel::addMode(const std::string& reqeusterNick, IChannelMode mode) {}
+IRC::Numeric Channel::addMode(const std::string& reqeusterNick,
+                              IChannelMode mode) {}
 
-int Channel::removeMode(const std::string& reqeusterNick, IChannelMode mode) {}
+IRC::Numeric Channel::removeMode(const std::string& reqeusterNick,
+                                 IChannelMode mode) {}
 
-IChannel::IChannelMode Channel::getMode() const {}
-
-int Channel::addToInviteList(const std::string& requesterNick,
+IRC::Numeric Channel::addToInviteList(const std::string& requesterNick,
                              const std::string& targetNick) {}
 
-int Channel::removeFromInviteList(const std::string& requesterNick,
+IRC::Numeric Channel::removeFromInviteList(const std::string& requesterNick,
                                   const std::string& targetNick) {}
 
 bool Channel::isInInviteList(const std::string& nick) const {}
