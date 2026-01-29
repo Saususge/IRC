fc6d69d1941d3c39835180c0676eda1b350c5268
yeonjunky <yeonjunky@gmail.com>
2026-01-29 19:21:52 +0900

    change IChannel, Channel function signiture

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -11,37 +11,35 @@ class Channel : private IChannel {
   const std::string& getChannelName() const;
 
   // Use `except` only for exclusing the sender
-  IRC::Numeric broadcast(const std::string& msg,
-                                 const std::string& except = "");
+  int broadcast(const std::string& msg, const std::string& except = "");
 
-  IRC::Numeric addClient(const std::string& nick);
+  IRC::Numeric addClient(const std::string& nick, const std::string& key = "");
   IRC::Numeric removeClient(const std::string& nick);
   bool hasClient(const std::string& nick) const;
   IRC::Numeric setClientOp(const std::string& requesterNick,
-                                   const std::string& targetNick);
+                           const std::string& targetNick);
   IRC::Numeric unsetClientOp(const std::string& requesterNick,
-                                     const std::string& targetNick);
+                             const std::string& targetNick);
   bool isClientOp(const std::string& nick) const;
   const std::vector<const std::string>& getClients();
 
   int getClientNumber() const;
 
-  IRC::Numeric setMode(const std::string& requesterNick,
-                               IChannelMode mode);
-  IRC::Numeric addMode(const std::string& requesterNick,
-                               IChannelMode mode);
-  IRC::Numeric removeMode(const std::string& requesterNick,
-                                  IChannelMode mode);
+  IRC::Numeric setMode(const std::string& requesterNick, IChannelMode mode);
+  IRC::Numeric addMode(const std::string& requesterNick, IChannelMode mode);
+  IRC::Numeric removeMode(const std::string& requesterNick, IChannelMode mode);
 
   IRC::Numeric addToInviteList(const std::string& requesterNick,
-                                       const std::string& targetNick);
+                               const std::string& targetNick);
   IRC::Numeric removeFromInviteList(const std::string& requesterNick,
-                                            const std::string& targetNick);
+                                    const std::string& targetNick);
   bool isInInviteList(const std::string& nick) const;
 
  private:
   std::string channelName;
-  IChannel::IChannelMode mode;
+  std::string topic;
+  std::string key;
+  IChannel::IChannelMode mode;  // inv, topic
   size_t limit;
 
   std::set<std::string> invitedUsers;
