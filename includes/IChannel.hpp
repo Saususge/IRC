c2d2f9adb94c0c8224c277c680a8c12b5d4f4f63
KHR416 <hyy741@naver.com>
2026-01-29 12:43:56 +0900

    Add requesterNick paramter for checking requester

--- includes/IChannel.hpp
+++ includes/IChannel.hpp
@@ -32,8 +32,10 @@ class IChannel {
   virtual int removeMode(const std::string& reqeusterNcik,
                          IChannelMode mode) = 0;
 
-  virtual int addToInviteList(const std::string& nick) = 0;
-  virtual int removeFromInviteList(const std::string& nick) = 0;
+  virtual int addToInviteList(const std::string& requesterNick,
+                              const std::string& targetNick) = 0;
+  virtual int removeFromInviteList(const std::string& requesterNick,
+                                   const std::string& targetNick) = 0;
   virtual bool isInInviteList(const std::string& nick) const = 0;
 };
 #endif
