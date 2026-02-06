0fb286164ca473da7631ea34e2bba94bb3f39627
yeonjunky <yeonjunky@gmail.com>
2026-02-06 19:41:34 +0900

    add empty getSession/getClient

--- srcs/ClientManagement.cpp
+++ srcs/ClientManagement.cpp
@@ -1,3 +1,5 @@
+#include "ClientManagement.hpp"
+
 #include "ClientRegistry.hpp"
 
 // ClientRegistry Singleton
@@ -9,8 +11,39 @@ namespace ClientManagement {
 ClientID createClient() { return _clientReg.createClient(); }
 void deleteClient(ClientID id) { _clientReg.deleteClient(id); }
 
-IClient* getClient(ClientID id) { return _clientReg.getClient(id); }
+IClient* getClient(ClientID id) {
+  (void)id;
+  return _clientReg.getClient(id);
+}
+
+IClient* getClient(SessionID sessionID) {
+  (void)sessionID;
+  return NULL;
+}
+
+IClient* getClient(ISession* session) {
+  (void)session;
+  return NULL;
+}
+
+IClient* getClient(const std::string& nick) {
+  (void)nick;
+  return NULL;
+}
+
 const std::set<const IClient*> getClients() { return _clientReg.getClients(); }
+
+ClientID getClientID(IClient* client) {
+  (void)client;
+  return ClientID(-1);
+}
+
+ClientID getClientID(const std::string& nick) {
+  (void)nick;
+  return ClientID(-1);
+}
+
+std::set<ClientID> getClientIDs() { return std::set<ClientID>(); }
 
 bool isNickinUse(const std::string& nick) {
   return _clientReg.isNickinUse(nick);
--- srcs/SessionManagement.cpp
+++ srcs/SessionManagement.cpp
@@ -1,3 +1,5 @@
+#include "SessionManagement.hpp"
+
 #include "Session.hpp"
 
 namespace {
@@ -7,6 +9,45 @@ SessionRegistry sessionReg;
 namespace SessionManagement {
 void addSession(ISession* session) { return sessionReg.addSession(session); }
 ISession* getSession(int socketFD) { return sessionReg.getSession(socketFD); }
+
+ISession* getSession(SessionID sessionID) {
+  (void)sessionID;
+  return NULL;
+}
+
+ISession* getSession(ClientID clientID) {
+  (void)clientID;
+  return NULL;
+}
+
+ISession* getSession(IClient* client) {
+  (void)client;
+  return NULL;
+}
+
+ISession* getSession(const std::string& nick) {
+  (void)nick;
+  return NULL;
+}
+
+std::set<ISession*> getSessions() { return std::set<ISession*>(); }
+
+SessionID getSessionID(ISession* session) {
+  (void)session;
+  return SessionID(-1);
+}
+
+SessionID getSessionID(IClient* client) {
+  (void)client;
+  return SessionID(-1);
+}
+
+SessionID getSessionID(const std::string& nick) {
+  (void)nick;
+  return SessionID(-1);
+}
+
+std::set<SessionID> getSessionIDs() { return std::set<SessionID>(); }
 
 void scheduleForDeletion(int socketFD) {
   sessionReg.scheduleForDeletion(socketFD);
